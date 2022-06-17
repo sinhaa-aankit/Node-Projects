356bd764c8db4184bb3cf9b44ddcd4e3361b883b
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2022-06-17 09:37:00 +0530

    Get and Post method by using File based System

--- api/app.js
+++ api/app.js
@@ -1,16 +1,50 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/',(req,res) => {
-	res.status(200)
-	    .json({message: 'Hello from server',
-	    	app: 'API' });
+// app.get('/',(req,res) => {
+// 	res.status(200)
+// 	    .json({message: 'Hello from server',
+// 	    	app: 'API' });
+// });
+
+// app.post('/', (req,res) => {
+// 	res.send('Sending Post from server');
+// })
+
+const tours = JSON.parse(
+	fs.readFileSync(__dirname + '/dev-data/data/tours-simple.json')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+	res.status(200).json({
+		status: 'success',
+		results: tours.length,
+		data: {
+			tours
+		}
+	});
 });
 
-app.post('/', (req,res) => {
-	res.send('Sending Post from server');
-})
+app.post('/api/v1/tours', (req, res) => {
+	// console.log(req.body);
+	const newId = tours[tours.length - 1].id + 1;
+	const newTour = Object.assign( {id: newId}, req.body);
+
+	tours.push(newTour);
+	fs.writeFile(__dirname + '/dev-data/data/tours-simple.json', JSON.stringify(tours), err => {
+		res.status(201).json({
+			status: 'success',
+			data:{
+				tour: newTour
+			}
+		})
+	})
+
+	// res.send('Done');
+});
 
 const port = 3000;
 app.listen(port, ()=>{
