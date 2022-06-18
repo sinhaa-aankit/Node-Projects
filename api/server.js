f35265d50fbc617fc1a1e440b2df8a05827d632b
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2022-06-18 23:33:41 +0530

    Creating Schema and Documents

--- api/server.js
+++ api/server.js
@@ -10,19 +10,45 @@ const DB = process.env.DATABASE.replace(
 	process.env.DATABASE_PASSWORD
 );
 
+//Connection DB
 mongoose.connect(DB, {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useFindAndModify: false
-}).then(con => {
-	console.log(con.connections);
-	console.log('DB Connected');
+}).then(() => console.log('DB Connected'))
+
+
+//Schema
+const tourSchema = new mongoose.Schema({
+	name: {
+		type:String,
+		required: [true, 'A tour must have a name'],
+		unique: true
+	},
+	rating: {
+		type: Number,
+		default:4.5
+	},
+	price: {
+		type: Number,
+		required: [true, 'A price must have a price']
+	}
+});
+
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+	name: 'The Trees Hiker',
+	rating:4.7,
+	price: 497
+});
+
+testTour.save().then(doc => {
+	console.log(doc);
+}).catch(err => {
+	console.log('Error ', err);
 })
-
-
-
-
-
 
 //Listening to port ( starting Server );
 const port = 3000;
