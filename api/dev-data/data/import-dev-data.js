eaa8730271336bc821b03de0d387b1621d9fb263
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2022-09-03 11:33:44 +0530

    Importing data into Database from .json file

---
+++ api/dev-data/data/import-dev-data.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const fs = require('fs');
+const Tour = require('./../../models/tourModel');
+// const DB = process.env.DATABASE.replace(
+// 	'<PASSWORD>',
+// 	process.env.DATABASE_PASSWORD
+// );
+
+//Connection DB LOCAL
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connected'));
+
+// Reading JSON FILE
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA TO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded to DB');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted from DB');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] == '--import') {
+  importData();
+} else if (process.argv[2] == '--delete') {
+  deleteData();
+}
