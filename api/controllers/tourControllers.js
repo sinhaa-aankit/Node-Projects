c69dbe457cf35d53f731ca2588d5959c40fab9aa
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2022-09-08 21:10:01 +0530

    Implemented Pagination and Aliasing

--- api/controllers/tourControllers.js
+++ api/controllers/tourControllers.js
@@ -3,6 +3,13 @@ const Tour = require('./../models/tourModel');
 
 const app = express();
 
+exports.aliasTopTours = async (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingAverage,price';
+  req.query.fields = 'name,price,ratingAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     //Building Query
@@ -27,11 +34,9 @@ exports.getAllTours = async (req, res) => {
     let query = Tour.find(queryObj);
 
     // Sorting, If sort query is present
-    // console.log(req.query.sort);
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
-      console.log(sortBy);
     } else {
       //Deafult sorting
       query = query.sort('-createdAt');
@@ -44,6 +49,18 @@ exports.getAllTours = async (req, res) => {
     } else {
       //Default filtering
       query = query.select('-__v');
+    }
+
+    // Pagination, If page(Default 1) and limit(Default 10) query is present
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     // Method 2 - (Hardcoding of method 1)
--- api/routes/tourRoutes.js
+++ api/routes/tourRoutes.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const app = express();
 
-
 const tourController = require('./../controllers/tourControllers');
-
 
 const router = express.Router();
 
 router
-	.route('/')
-	.get(tourController.getAllTours)
-	.post(tourController.createTour);
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
-	.route('/:id')
-	.get(tourController.getTour)
-	.patch(tourController.updateTour)
-	.delete(tourController.deleteTour);
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
