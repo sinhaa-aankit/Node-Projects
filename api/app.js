43b849f19aa838138e3e09810831ff432b88de98
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2022-09-19 00:14:31 +0530

    1. Handled Unhandled Routes. 2. Global Error Handling Middleware. 3. Better Error Handling & Refactoring

--- api/app.js
+++ api/app.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -13,16 +16,19 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  // console.log('Hello from middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use('/api/v1/tours', tourRouter); // mounting multiple tours routes
 app.use('/api/v1/users', userRouter); // mounting multiple users routes
+
+//Unhandled Routes
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+//Global Error handling middleware
+app.use(globalErrorHandler);
 
 module.exports = app;
