8b4a1c706024ae0250398e147d789134cbf04d1f
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2022-09-22 20:14:52 +0530

    Handled Validation Errors.

--- api/controllers/errorController.js
+++ api/controllers/errorController.js
@@ -12,6 +12,12 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -51,11 +57,11 @@ module.exports = (err, req, res, next) => {
     if (error.name === 'CastError') {
       error = handleCastErrorDB(error);
     }
-    // if (error.code === 11000) {
-    //   error = handleDuplicateFieldsDB(error);
-    // }
     if (error.code === 11000) {
       error = handleDuplicateFieldsDB(error);
+    }
+    if (error._message === 'Validation failed') {
+      error = handleValidationErrorDB(error);
     }
     sendErrorProd(error, res);
   }
