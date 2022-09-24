15c75af36455b1d84b0530a7e561615b13ed4263
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2022-09-24 21:42:08 +0530

    Errors outside express: Unhandled Rejections and Uncaught Exceptions.

--- api/server.js
+++ api/server.js
@@ -4,12 +4,22 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+//Uncaught Exception
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  process.exit(1);
+});
+
+// console.log(x);
+
 // const DB = process.env.DATABASE.replace(
-// 	'<PASSWORD>',
-// 	process.env.DATABASE_PASSWORD
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
 // );
 
 //Connection DB LOCAL
+// .connect(process.env.DATABASE_LOCAL, {
 mongoose
   .connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
@@ -21,6 +31,14 @@ mongoose
 
 //Listening to port ( starting Server );
 const port = 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is up and runnign on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
