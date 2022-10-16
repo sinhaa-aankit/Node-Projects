cd139609269dc768aa1fe7d426c02604e13b08a1
Ankit Kumar Sinha <40895035+sinhaa-aankit@users.noreply.github.com>
2022-10-16 19:34:34 +0530

    Implemented the Update Email and Name functionality for the Currently Logged In User.

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -2,6 +2,15 @@ const express = require('express');
 const catchAsync = require('../utils/catchAsync');
 const app = express();
 const User = require('./../models/userModel');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const user = await User.find();
@@ -10,6 +19,29 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     data: user,
   });
 });
+
+exports.updateMe = async (req, res, next) => {
+  // 1 Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for password updates.', 400));
+  }
+
+  // 2 Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 2 Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    message: ' success ',
+    data: {
+      user: updatedUser,
+    },
+  });
+};
 
 exports.getUser = (req, res) => {
   res.status(500).json({
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -18,6 +18,8 @@ router.patch(
   authController.updatePassword
 );
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 router
   .route('/')
   .get(userController.getAllUsers)
